76c8d16deae955a9881727d7f02ec8a1baa1125a
KingsDev <66224535+kingsDev-dev@users.noreply.github.com>
2020-06-07 16:33:32 +0100

    kingsley.js-task2
    task2

---
+++ Abejashi.js
@@ -0,0 +1,9 @@
+let name = "Abejashi";
+let ID = "HNGi7 - 00823";
+let language = "Javascript";
+let email = "kings.ks266@gmail.com";
+
+function task2 (name, id, language, email,){ 
+    console.log(Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email});
+    }
+    task2(name, id, language, email);
