20fb80e1660e30df1835bd3f0c64e8f9148672a8
Abejashi Kingsley <kings.ks266@gmail.com>
2020-06-09 01:37:53 +0100

    first commit

---
+++ Desktop/kingsley.js/script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+<html>
+<body>
+
+var = name;
+name = "Abejashi kingsley"; id = "HNGi7-00823"; language = "Javascript"; email="kings.ks266@gmail.com";
+
+function (name, id, language, email,){
+     console.log(Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email});
+     }
+     task2(name, id, language, email);
+     
+ </body>
+ </html>    
